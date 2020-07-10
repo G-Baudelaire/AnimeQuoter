f3b1a75979ed746fe76af449779214814de7cfae
YeetmeisterII <germainjones1@gmail.com>
2020-07-10 18:56:56 +0100

    Add test methods.
    Additions:
    - Add tests for get_quote methods.
    
    Changes:
    - Renamed commands.
    - Fix bug at line 20 of dbquery.py due to having wrong url.
    - Fix writing to error_log.txt caused by erroneous validation.

--- core/bot.py
+++ core/bot.py
@@ -13,19 +13,19 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 bot = Bot(command_prefix="!quote ")
 
 
-@bot.command(name="random", help="Responds with a quote from a random character from a random anime.")
+@bot.command(name="rand", help="Responds with a quote from a random character from a random anime.")
 async def random_quote(ctx):
     quote = await dbquery.get_random_quote(client)
     await ctx.send(quote)
 
 
-@bot.command(name="character", help="Responds with a random quote from a random anime character.")
+@bot.command(name="char", help="Responds with a random quote from a random anime character.")
 async def character_quote(ctx, *args: str):
     quote = await dbquery.get_character_quote(client, " ".join(args))
     await ctx.send(quote)
 
 
-@bot.command(name="anime", help="Responds with a random quote from an anime.")
+@bot.command(name="ani", help="Responds with a random quote from an anime.")
 async def anime_quote(ctx, *args: str):
     quote = await dbquery.get_anime_quote(client, " ".join(args))
     await ctx.send(quote)
@@ -37,4 +37,5 @@ async def on_command_error(ctx, error):
         await ctx.send("Command does not exist.")
 
 
-bot.run(TOKEN)
+if __name__ == '__main__':
+    bot.run(TOKEN)
--- core/tests/dbquery_tests.py
+++ core/tests/dbquery_tests.py
@@ -3,10 +3,10 @@ import unittest
 
 import aiohttp
 
-from core.dbquery import get_random_quote, get_character_quote
+from core.dbquery import get_random_quote, get_character_quote, get_anime_quote
 
 
-class TestDBQueryMethods(unittest.TestCase):
+class CustomTestCase(unittest.TestCase):
     @classmethod
     async def _open_client(cls):
         return aiohttp.ClientSession()
@@ -17,24 +17,52 @@ class TestDBQueryMethods(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        print("S")
-        cls._loop = asyncio.get_event_loop()
+        cls._loop = asyncio.new_event_loop()
         cls._client = cls._loop.run_until_complete(cls._open_client())
 
     @classmethod
     def tearDownClass(cls) -> None:
-        print("T")
         cls._loop.run_until_complete(cls._client.close())
         cls._loop.close()
 
+
+class TestDBQueryGetRandomQuoteMethod(CustomTestCase):
     def test_get_random_quote(self):
         output = self._loop.run_until_complete(get_random_quote(self._client))
         self.assertNotIn(output, ("There was an error while retrieving quote.", "No quotes found."))
 
-    def test_get_character_quote_blank(self):
+
+class TestDBQueryGetCharacterQuoteMethod(CustomTestCase):
+    def test_blank_character(self):
         output = self._loop.run_until_complete(get_character_quote(self._client, ""))
         self.assertEqual("Character must be specified.", output)
 
-    def test_get_character_quote_charater(self):
+    def test_non_existent_character(self):
+        output = self._loop.run_until_complete(get_character_quote(self._client, "akldjflkadklaf;ldkadsgdasggs"))
+        self.assertEqual("No quotes found.", output)
+
+    def test_url_character(self):
+        output = self._loop.run_until_complete(get_character_quote(self._client, "https://github.com/"))
+        self.assertEqual("No quotes found.", output)
+
+    def test_real_character(self):
         output = self._loop.run_until_complete(get_character_quote(self._client, "Sora"))
-        self.assertNotEqual("Character not specified.", output)
+        self.assertNotIn(output, ("Character must be specified.", "No quotes found."))
+
+
+class TestDBQueryGetAnimeQuoteMethod(CustomTestCase):
+    def test_blank_anime(self):
+        output = self._loop.run_until_complete(get_anime_quote(self._client, ""))
+        self.assertEqual("Anime must be specified.", output)
+
+    def test_non_existent_anime(self):
+        output = self._loop.run_until_complete(get_anime_quote(self._client, "akldjflkadklafldkadsgdasggs"))
+        self.assertEqual("No quotes found.", output)
+
+    def test_url_anime(self):
+        output = self._loop.run_until_complete(get_anime_quote(self._client, "https://github.com/"))
+        self.assertEqual("No quotes found.", output)
+
+    def test_valid_anime(self):
+        output = self._loop.run_until_complete(get_anime_quote(self._client, "Fairy Tail"))
+        self.assertNotIn(output, ("Anime must be specified.", "No quotes found."))
